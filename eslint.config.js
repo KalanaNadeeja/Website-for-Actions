af9642487f3621701dacf5676424cdbe037ffac5
Kalana Nadija <152647512+KalanaNadeeja@users.noreply.github.com>
2025-03-09 08:35:02 +0530

    pint

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
 export default [
     {
-      files: ["**/*.js", "**/*.jsx"], 
+      files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"], 
       languageOptions: {
         ecmaVersion: "latest",
         sourceType: "module",
